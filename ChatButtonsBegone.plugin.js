37d081224b7dd7d19ff7b1696fe938042d91bc46
Zacam <zacam.1369@gmail.com>
2026-03-31 18:04:46 -0700

    Add: Hide Profile Banner
    Adds "Hide Profile Banner" to Profile Customizations options as something to remove.

--- ChatButtonsBegone.plugin.js
+++ ChatButtonsBegone.plugin.js
@@ -541,6 +541,13 @@ const config = {
                 },
                 {
                     type: 'switch',
+                    id: 'hideBanner',
+                    name: 'Remove Profile Banner',
+                    note: 'Removes the banner image from user profiles.',
+                    value: false,
+                },
+                {
+                    type: 'switch',
                     id: 'profileEffects',
                     name: 'Remove Profile Effects',
                     note: 'Removes profile effects (Animated Overlays) from user profiles.',
@@ -765,6 +772,7 @@ module.exports = class ChatButtonsBegone {
             this.avatar,
             this.avatarDecorationChat,
             this.profileBadges,
+            this.profileBanner,
             this.profileEffects,
             this.profileGIF,
             this.profileCollection,
@@ -836,6 +844,7 @@ module.exports = class ChatButtonsBegone {
             { filter: this.api.Webpack.Filters.byKeys('avatarDecorationContainer') }, // Avatar Decoration
             { filter: this.api.Webpack.Filters.byKeys('avatarDecoration','contents') }, // Avatar Decoration in Chat
             { filter: this.api.Webpack.Filters.byKeys('tags','usernameRow') }, // Profile Badges
+            { filter: this.api.Webpack.Filters.byKeys('mask','banner') }, // Profile Badges
             { filter: this.api.Webpack.Filters.byKeys('profileEffects') }, // Profile Effects
             { filter: this.api.Webpack.Filters.byKeys('mask', 'gifTag') }, // Profile GIF Tag
             { filter: this.api.Webpack.Filters.byKeys('cardsList', 'firstCardContainer') }, // Profile Game Collection
@@ -1136,6 +1145,7 @@ module.exports = class ChatButtonsBegone {
         }
 
         if (this.settings.profileCustomizations.hideBadges) this.styler.add(`.${this.profileBadges.tags} > div:has(a)`);
+        if (this.settings.profileCustomizations.hideBanner) this.styler.add(`.${this.profileBanner.banner}`);
         if (this.settings.profileCustomizations.profileEffects) this.styler.add(`.${this.profileEffects.profileEffects} .${this.profileEffects.effect}`);
         if (this.settings.profileCustomizations.profileGIF) this.styler.add(`.${this.profileGIF.gifTag}`);
         if (this.settings.profileCustomizations.hideCollection) {
